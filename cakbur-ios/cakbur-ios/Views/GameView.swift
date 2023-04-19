fd4bdbd70cf1493f3d87aafa96d88f24c86628c1
hanselmediatropy <hansel.matthew@mediatropy.com>
2023-04-19 09:22:34 +0700

    feat: create cakbur swiftui app project

--- cakbur-ios/cakbur-ios/Views/GameView.swift
+++ cakbur-ios/cakbur-ios/Views/GameView.swift
@@ -8,25 +8,27 @@
 import SwiftUI
 import SpriteKit
 
+import SwiftUI
+import SpriteKit
+
 struct GameView: View {
     
     @AppStorage("shouldShowOnboarding") var shouldShowOnboarding = true
     
-    @StateObject var scene: GameScene = {
-        let scene = GameScene()
-        return scene
-    }()
+    @StateObject var scene = GameScene()
     
     var body: some View {
         Group {
             if scene.score == 5 {
                 WinnerView(score: $scene.score)
+                    .transition(.opacity)
             } else if !shouldShowOnboarding {
                 SpriteView(scene: scene)
                     .edgesIgnoringSafeArea(.all)
                     .transition(.opacity)
             } else {
                 OnboardingView(shouldShowOnboarding: $shouldShowOnboarding)
+                    .transition(.opacity)
             }
         }
     }
